d7da4486576319bdf8e347b42b1667993181ed94
Sagita Narayana <sagita.ketut213@gmail.com>
2024-07-21 11:02:57 +0800

    token fix

--- laravel/app/Http/Controllers/Auth/LogoutController.php
+++ laravel/app/Http/Controllers/Auth/LogoutController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-use App\Models\User;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LogoutController extends Controller
 {
@@ -12,8 +13,17 @@ class LogoutController extends Controller
      */
     public function __invoke(Request $request)
     {
-        // Invalidate the token by deleting it
-        $request->user()->currentAccessToken()->delete();
-        return response()->json(['message' => 'Logout successful'], 200);
+        // Retrieve the authenticated user
+        $user = Auth::user();
+
+        if ($user) {
+            // Invalidate the remember_token by setting it to null
+            $user->remember_token = null;
+            $user->save();
+
+            return response()->json(['message' => 'Logout successful'], 200);
+        }
+
+        return response()->json(['message' => 'User not authenticated'], 401);
     }
 }
